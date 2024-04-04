2ec9058e11d5d5864c5e20543e5c5368bb28a184
mne30 <kondratiev.r.a@yandex.ru>
2024-04-04 16:12:25 +0300

    Recursion HomeWork

--- recursion/Program.cs
+++ recursion/Program.cs
@@ -1,4 +1,11 @@
 ﻿using CsharpMethods;
 
-Recursion recurs = new Recursion(4);
-recurs.rec(5);
+Recursion recurs = new Recursion();
+int[] intArray = { 1, 2, 3, 4, 5, 8, 12, 17};
+//recurs.rec(5);
+Console.WriteLine("\n Task 1:");
+recurs.MToN(10, 23);
+Console.WriteLine("\n Task 2:");
+Console.WriteLine(recurs.AkkermanFunction(2, 3));
+Console.WriteLine(" \n Task 3:");
+recurs.printRecArrayVer1(intArray, intArray.Length - 1);
--- recursion/Recursion.cs
+++ recursion/Recursion.cs
@@ -1,16 +1,48 @@
-namespace CsharpMethods{
-    public class Recursion{
-        private int a;
-        public Recursion(int a){
-            this.a = a;
-        }
-        public void rec(int n){
-            if( n == 10){
+namespace CsharpMethods
+{
+    public class Recursion
+    {
+        // public Recursion(){       
+        // }
+        public void rec(int n)
+        {
+            if (n == 10)
+            {
                 return;
             }
             Console.WriteLine($" Выыодим число: {n} ");
-            n++;
-            rec(n);
+            rec(n + 1);
         }
+
+        // Урок 7. Рекурсия
+
+        // Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+        public void MToN(int m, int n)
+        {
+            if (m > n) return;
+            Console.Write(m + " ");
+            MToN(m + 1, n);
+        }
+
+        // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+        // A(m - 1, A(m, n - 1)) 
+        // Возвращенный 0 будет исключением
+        public int AkkermanFunction(int m, int n){
+            if(m < 0 || n < 0) return 0;
+            //Комбинирую тернарныеые выражения
+            return m == 0 ? n + 1 : ( m > 0 && n == 0 ? AkkermanFunction(m - 1, 1) : AkkermanFunction(m - 1, AkkermanFunction(m, n - 1)));
+            //Реализация на if
+            // if(m == 0) return n + 1;
+            // if(m > 0 && n == 0) return AkkermanFunction(m - 1, 1);
+            // return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+        }
+        // Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+        public void printRecArrayVer1(int[] intArray, int count){
+            if(count < 0) return;
+            Console.WriteLine(intArray[count]);
+            printRecArrayVer1(intArray, count - 1);
+        }
+
+
     }
 }
